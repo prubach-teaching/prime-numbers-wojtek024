b62f512e60033b7ab107b506cadfe301bdd7b355
wojtek024 <61975407+wojtek024@users.noreply.github.com>
2020-04-27 14:58:41 +0200

    Update PrimeNumbers.java

--- src/main/java/sgh/PrimeNumbers.java
+++ src/main/java/sgh/PrimeNumbers.java
@@ -1,13 +1,39 @@
 package sgh;
 
-public class PrimeNumbers {
-
+package pl.waw.student;
+public class PrimesTest {
     public static void primes(int n) {
-        // Your code here
-        System.out.println("2, 3, 5, 7");
+        int liczbaPierwsza = 1;
+        //System.out.println("Pierwsze"+ n+" liczb pierwszych to: ");
+        String output = "";
+        for (int k=1;k<=n;k++) {
+            liczbaPierwsza = nastepnaLiczbaPierwsza(liczbaPierwsza);
+            //System.out.println(k+" liczba pierwsza to: "+liczbaPierwsza);
+            output += liczbaPierwsza + ", ";
+            //System.out.print(liczbaPierwsza + ", ");
+        }
+        String outtext = output.substring(0, output.lastIndexOf(","));
+        System.out.println(outtext);
     }
 
-
+    public static void main(String[] args) {
+      primes(9);
+    }
+    public static int nastepnaLiczbaPierwsza(int liczba){
+        int wynik = liczba+1;
+        int dzielnik = 2;
+        while(dzielnik < wynik){
+            if(wynik % dzielnik == 0){
+                wynik ++;
+                dzielnik = 2;
+            }
+            else {
+                dzielnik ++;
+            }
+        }
+        return wynik;
+    }
+}
     public static void main(String[] args) {
         primes(4);
     }
